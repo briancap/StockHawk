94eef9fe722ddb596eb760d33d3fe486baed6c87
briancap <btc5081@gmail.com>
2017-02-07 16:32:14 -0500

    make the graph labels work
    added dates to the graph

--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
@@ -3,16 +3,19 @@ package com.udacity.stockhawk.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 
 import butterknife.BindView;
@@ -28,16 +31,16 @@ public class StockDetailActivity extends AppCompatActivity  {
 
     @BindView(R.id.graph_view)
     GraphView graphView;
+    @BindView(R.id.stock_symbol)
+    TextView stockSymbolTV;
 
     HashMap<Object, Object> singleStock;
-    HashMap<Long, Double> stockHistory = new HashMap<>();
-    ArrayList<Long> historyDatesForSorting = new ArrayList<>();
+    HashMap<Date, Double> stockHistory = new HashMap<>();
+    ArrayList<Date> historyDatesForSorting = new ArrayList<>();
 
     String stockSymbol;
     String currentStockPrice;
     String historyString;
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,30 +49,31 @@ public class StockDetailActivity extends AppCompatActivity  {
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
-        if(intent != null){
+        if(intent != null) {
             singleStock = (HashMap) intent.getSerializableExtra("data");
 
-            stockSymbol         = singleStock.get(Contract.Quote.COLUMN_SYMBOL).toString();
-            currentStockPrice   = singleStock.get(Contract.Quote.COLUMN_PRICE).toString();
-            historyString       = singleStock.get(Contract.Quote.COLUMN_HISTORY).toString();
+            stockSymbol = singleStock.get(Contract.Quote.COLUMN_SYMBOL).toString();
+            currentStockPrice = singleStock.get(Contract.Quote.COLUMN_PRICE).toString();
+            historyString = singleStock.get(Contract.Quote.COLUMN_HISTORY).toString();
+
+            stockSymbolTV.setText(stockSymbol);
 
             formatHistoryData(historyString);
-            
+
+            DataPoint[] dataPoints = new DataPoint[historyDatesForSorting.size()];
+
+            for (int i = 0; i < historyDatesForSorting.size(); i++) {
+                dataPoints[i] = new DataPoint(
+                        historyDatesForSorting.get(i)
+                        , stockHistory.get(historyDatesForSorting.get(i))
+                );
+            }
+
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
+            graphView.addSeries(series);
+            graphView.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+            graphView.getGridLabelRenderer().setNumHorizontalLabels(3);
         }
-
-        DataPoint[] dataPoints = new DataPoint[historyDatesForSorting.size()];
-
-        for(int i = 0; i < historyDatesForSorting.size(); i++){
-            dataPoints[i] = new DataPoint(
-                    historyDatesForSorting.get(i)
-                    , stockHistory.get(historyDatesForSorting.get(i))
-            );
-        }
-
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
-
-        graphView.addSeries(series);
-
     }
 
     public void formatHistoryData(String historyString){
@@ -80,14 +84,25 @@ public class StockDetailActivity extends AppCompatActivity  {
 
         for(int i = 0; i < pricePerDay.length; i++){
             String[] singleDay = pricePerDay[i].toString().split(","); //split the day from the price using the comma
+            Long millis = Long.parseLong(singleDay[0].trim()); //0 = date, 1 = stock price
+            Double stockPrice = Double.parseDouble(singleDay[1].trim());
 
-            stockHistory.put(Long.parseLong(singleDay[0].trim()), Double.parseDouble(singleDay[1].trim()));//0 = date, 1 = stock price
-            historyDatesForSorting.add(Long.parseLong(singleDay[0].trim()));
+            stockHistory.put(getDateFromMiliseconds(millis), stockPrice);
+            historyDatesForSorting.add(getDateFromMiliseconds(millis));
 
         }
 
         Collections.reverse(historyDatesForSorting); //want the graph to read left to right an we feed the data in right to left
 
     }
+
+    public Date getDateFromMiliseconds(Long milli){;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milli);
+
+        return calendar.getTime();
+    }
+
+
 
 }
